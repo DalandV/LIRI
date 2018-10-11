cc23347ebdcb5dd51a28fd6735dc3060554c0502
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2018-10-11 13:10:45 -0400

    Bands in Town API works

--- liri.js
+++ liri.js
@@ -1,3 +1,7 @@
+
+var request = require('request');
+var moment = require('moment');
+
 // read and set any environment variables with the dotenv package
 require("dotenv").config();
 
@@ -26,7 +30,7 @@ if (userCommand === "spotify-this-song" && process.argv.length === 4) {
         console.log("Album: " + data.tracks.items[0].album.name + "\n");
     });
 }
-else {
+else if (userCommand === "spotify-this-song" && process.argv.length === 3) {
     spotify.search({ type: 'track', query: "The Sign" }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
@@ -42,3 +46,44 @@ else {
         console.log("Album: " + data.tracks.items[7].album.name + "\n");
     });
 }
+
+if (userCommand === "concert-this" && process.argv.length === 4) {
+
+    request("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp", function (error, response) {
+        if (error) {
+            console.log('error:', error);
+        }
+
+        var info = JSON.parse(response.body);
+        // console.log(info);
+        console.log("\n" + "CONCERT INFO" + "\n");
+
+        for (var i = 0; i < info.length; i++) {
+            console.log("Venue: " + info[i].venue.name);
+            if (info[i].venue.region === '') {
+                console.log("City: " + info[i].venue.city + ", " + info[i].venue.country)
+            }
+            else {
+                console.log("City: " + info[i].venue.city + ", " + info[i].venue.region);
+            }
+            console.log(moment(info[i].datetime).format("MMMM Do YYYY"));
+            console.log("\n")
+        }
+    });
+}
+else if (userCommand === "concert-this" && process.argv.length === 3){
+    console.log("\nOops! Looks like you forgot to include which artist/band you're searching for.\n")
+}
+
+if (userCommand === "movie-this" && process.argv.length === 4) {
+
+    request("http://www.omdbapi.com/?apikey=trilogy&r=json&t=" + userInput, function (error, response) {
+        if (error) {
+            console.log('error:', error);
+        }
+
+        var info = JSON.parse(response.body);
+        console.log(info.Title);
+    });
+}
+
